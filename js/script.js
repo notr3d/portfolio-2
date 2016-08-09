3906186a6c1f4e61c79287bde4bcb93a845d3fff
Александр Некрасов <lxmrrr@gmail.com>
2016-08-09 17:57:08 +0300

    09.08

--- js/script.js
+++ js/script.js
@@ -4,24 +4,42 @@ workItem.click(function(){
 	if (!popup.hasClass('popup--visible')) {
 		popup.addClass('popup--visible');
 	};
-	var popupClose = popup.find('.popup__close');
-	popupClose.click(function(){
-		popup.removeClass('popup--visible');
-	});
 	popup.click(function(){
 		popup.removeClass('popup--visible');
 	});
 	popup.find('.popup__inner').click(function(e) {
 		e.stopPropagation();
 	});
+	var popupClose = popup.find('.popup__close');
+	popupClose.click(function(){
+		popup.removeClass('popup--visible');
+	});
 })
 
 $(document).keyup(function(e) {
-     if (e.keyCode == 27) { // escape key maps to keycode `27`
+     if (e.keyCode == 27) { 
         $('.popup--visible').removeClass('popup--visible');
     }
 });
 
-$('.popup--visible').click(function(){
-	alert(1)
+var navItem = $('.site-nav').children();
+var pages = $('.page-container').children(); 
+navItem.click(function(){
+	$(this).addClass('site-nav__item--active');
+	$(this).siblings().removeClass('site-nav__item--active');
+	var id = $(this).index();
+	var currentPage = pages.eq(id);
+	currentPage.addClass('page--active');
+	currentPage.siblings().removeClass('page--active');
+});
+
+var siteHeader = $('.site-header');
+var siteHeaderActiveState = 0;
+$(window).scroll(function(){
+	var scrollTop = $(window).scrollTop();
+	if (scrollTop > siteHeaderActiveState) {
+		siteHeader.addClass('site-header--fixed');
+	} else {
+		siteHeader.removeClass('site-header--fixed')
+	};
 });
